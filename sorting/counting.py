b0314ce9533da14be2fb36685929bb179295fcc7
Akhil Chauhan <109506738+akhil616@users.noreply.github.com>
2024-06-04 21:58:06 +0530

    Update counting.py

--- sorting/counting.py
+++ sorting/counting.py
@@ -1,26 +1,17 @@
-def count_sort(arr):
+def countSort(arr):
     m = max(arr)
     tmp = [0] * (m + 1)
     for i in range(len(arr)):
         tmp[arr[i]] += 1
-    print(tmp)
     idx = 0
-    i = 0
-    while i < m + 1:
+    for i in range(m + 1):
         while tmp[i] != 0:
             arr[idx] = i
-            idx += 1
             tmp[i] -= 1
-        i += 1    
-    return arr
-
-# Driver code
+            idx += 1
+    
 if __name__ == "__main__":
-	# Input array
-	input_array = [4, 3, 12, 1, 5, 5, 3, 9]
-
-	# Output array
-	output_array = count_sort(input_array)
-
-	for num in output_array:
-		print(num, end=" ")
+    arr = [5,123,3,13,5,6,4,7,9,100]
+    print('array before sorting: ', arr)
+    countSort(arr)
+    print('array after sorting: ', arr)
