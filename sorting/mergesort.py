bb0667933cb61f0fdf2d4a3dbe35382c13b124f2
Akhil Chauhan <109506738+akhil616@users.noreply.github.com>
2024-06-04 21:37:38 +0530

    Update mergesort.py

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -35,3 +35,45 @@ class Solution:
             self.mergeSort(arr, l, mid)
             self.mergeSort(arr, mid + 1, r)
             self.merge(arr, l, mid, r)
+
+# Raw code
+def merge(arr, l, m, r):
+    tmp = [0] * (r - l + 1)
+    left, right, k = l, m + 1, 0
+    
+    while left <= m and right <= r: 
+        if arr[left] <= arr[right]:
+            tmp[k] = arr[left]
+            left += 1
+        else:
+            tmp[k] = arr[right]
+            right += 1
+        k += 1
+    
+    while left <= m:
+        tmp[k] = arr[left]
+        left += 1
+        k += 1
+    
+    while right <= r:
+        tmp[k] = arr[right]
+        right += 1
+        k += 1
+    
+    for i in range(k):
+        arr[l + i] = tmp[i]
+
+def mergeSort(arr, l, r):
+    if l < r:
+        mid = (l + r) // 2
+        mergeSort(arr, l, mid)
+        mergeSort(arr, mid + 1, r)
+        merge(arr, l, mid, r)
+        
+if __name__ == "__main__":
+    arr = [5,123,3,13,5,6,4,7,9,100]
+    print('array before sorting: ', arr)
+    n = len(arr)
+    l, r = 0, n - 1
+    mergeSort(arr, l, r)
+    print('array after sorting: ', arr)
